be1f309da4d574647b6bb565072a344b36051c7b
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2008-10-22 23:51:18 +0000

    [maven-release-plugin]  copy for tag io-tools-0.1

---
+++ tags/io-tools-0.1/src/main/java/org/iotools/formats/FormatEnum.java
@@ -0,0 +1,37 @@
+package org.iotools.formats;
+
+/*
+ * Copyright (c) 2008, Davide Simonetti
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, 
+ * with or without modification, are permitted provided that the following 
+ * conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of Davide Simonetti nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without 
+ *    specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY 
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+/**
+ * Enum of detected formats. Some format is "simple", some other is just a way
+ * of encoding another kind of content.
+ * 
+ * @author dvd.smnt
+ */
+public enum FormatEnum {
+	BASE64, M7M, PDF, PEM, PKCS7, TIMESTAMP, UNKNOWN, XML, ZIP
+}
