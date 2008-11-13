5e14ccb1b20e8c4e9d2d19267b7b024921c0d2f3
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2008-11-13 03:01:36 +0000

    java 1.4 compliance. junits are green.

--- trunk/wazstream/src/test/java/org/iotools/formats/detectors/TestDetectors.java
+++ trunk/wazstream/src/test/java/org/iotools/formats/detectors/TestDetectors.java
@@ -1,11 +1,9 @@
 package org.iotools.formats.detectors;
 
-import static org.junit.Assert.assertTrue;
+import junit.framework.TestCase;
 
-import org.junit.Test;
+public class TestDetectors extends TestCase {
 
-public class TestDetectors {
-	@Test
 	public void testXmlDetector() {
 		assertTrue("short xml detected", new XmlDetector()
 				.detect("<xml>this is xml</xml>".getBytes()));
