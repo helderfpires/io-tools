ef56c2879059928ca28fe4a6054adf941975047b
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2008-11-14 00:14:40 +0000

    rename project to wazformat

---
+++ wazformat/src/test/java/org/googlecode/iotools/fmt/TestGuessInputStream.java
@@ -0,0 +1,74 @@
+package org.iotools.formats;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
+import org.iotools.formats.base.FormatEnum;
+
+public class TestGuessInputStream extends TestCase {
+
+	public void testDecoder() throws IOException {
+		final InputStream istream = TestGuessInputStream.class
+				.getResourceAsStream("/testFiles/test_pdf.pdf.b64");
+		final byte[] reference = IOUtils.toByteArray(istream);
+
+		final InputStream istream2 = TestGuessInputStream.class
+				.getResourceAsStream("/testFiles/test_pdf.pdf.b64");
+		final GuessInputStream gis = GuessInputStream.getInstance(istream2,
+				new FormatEnum[] { FormatEnum.BASE64, FormatEnum.PDF });
+		assertEquals("Format detected", FormatEnum.BASE64, gis.getFormat());
+		assertTrue("Bytes read are same", Arrays.equals(reference, IOUtils
+				.toByteArray(gis)));
+		assertEquals("Detected formats", 2, gis.getFormats().length);
+		assertEquals("Format [0]", FormatEnum.BASE64, gis.getFormats()[0]);
+		assertEquals("Format [1]", FormatEnum.PDF, gis.getFormats()[1]);
+	}
+
+	public void testEnabledFormat() throws IOException {
+		final InputStream istream = TestGuessInputStream.class
+				.getResourceAsStream("/testFiles/test_pdf.pdf");
+		final byte[] reference = IOUtils.toByteArray(istream);
+
+		final InputStream istream2 = TestGuessInputStream.class
+				.getResourceAsStream("/testFiles/test_pdf.pdf");
+		final GuessInputStream gis = GuessInputStream.getInstance(istream2,
+				new FormatEnum[] { FormatEnum.PDF });
+		assertEquals("Format detected", FormatEnum.PDF, gis.getFormat());
+		assertTrue("Bytes read are same", Arrays.equals(reference, IOUtils
+				.toByteArray(gis)));
+		assertEquals("Detected formats", 1, gis.getFormats().length);
+
+	}
+
+	public void testNotEnabledFormat() throws IOException {
+		final InputStream istream = TestGuessInputStream.class
+				.getResourceAsStream("/testFiles/test_pdf.pdf");
+		final byte[] reference = IOUtils.toByteArray(istream);
+
+		final InputStream istream2 = TestGuessInputStream.class
+				.getResourceAsStream("/testFiles/test_pdf.pdf");
+		final GuessInputStream gis = GuessInputStream.getInstance(istream2,
+				new FormatEnum[] { FormatEnum.XML });
+		assertEquals("Format detected", FormatEnum.UNKNOWN, gis.getFormat());
+		assertTrue("Bytes read are same", Arrays.equals(reference, IOUtils
+				.toByteArray(gis)));
+		assertEquals("Detected formats", 1, gis.getFormats().length);
+	}
+
+	public void testShortFile() throws IOException {
+		final InputStream istream = new ByteArrayInputStream(
+				"<xml>this is xml</xml>".getBytes());
+		final byte[] reference = IOUtils.toByteArray(istream);
+		istream.reset();
+		final GuessInputStream gis = GuessInputStream.getInstance(istream,
+				new FormatEnum[] { FormatEnum.XML });
+		assertEquals("Format detected", FormatEnum.XML, gis.getFormat());
+		assertTrue("Bytes read are same", Arrays.equals(reference, IOUtils
+				.toByteArray(gis)));
+	}
+}
