3113b45751d286b8f3151385e2a5590ac3c4bfc3
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2008-11-24 22:19:50 +0000

    package rename (sorry...) added SizeReaderInputStream and SizeRecorderOutputStream

---
+++ easystream/src/test/java/com/gc/iotools/stream/is/ReadSizeInputStreamTest.java
@@ -0,0 +1,80 @@
+package com.gc.iotools.stream.is;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.NullOutputStream;
+
+import com.gc.iotools.stream.is.SizeReaderInputStream;
+
+public class ReadSizeInputStreamTest {
+
+	@org.junit.Test
+	public void fullReadOnClose() throws IOException {
+		final InputStream istream = ReadSizeInputStreamTest.class
+				.getResourceAsStream("/testFile.txt");
+		final SizeReaderInputStream ris = new SizeReaderInputStream(istream,
+				true);
+		ris.read(new byte[5]);
+		assertEquals("readed size", 5, ris.getSize());
+		ris.close();
+		assertEquals("in the end", 30, ris.getSize());
+	}
+
+	@org.junit.Test
+	public void markAndReset() throws Exception {
+		final InputStream istream = ReadSizeInputStreamTest.class
+				.getResourceAsStream("/testFile.txt");
+		final SizeReaderInputStream ris = new SizeReaderInputStream(istream,
+				true);
+		ris.read(new byte[5]);
+		assertEquals("readed size", 5, ris.getSize());
+		ris.mark(50);
+		ris.read(new byte[10]);
+		assertEquals("read after mark", 15, ris.getSize());
+		ris.reset();
+		assertEquals("read after reset", 5, ris.getSize());
+		ris.close();
+		assertEquals("in the end", 30, ris.getSize());
+	}
+
+	@org.junit.Test
+	public void noFullReadOnClose() throws IOException {
+		final InputStream istream = ReadSizeInputStreamTest.class
+				.getResourceAsStream("/testFile.txt");
+		final SizeReaderInputStream ris = new SizeReaderInputStream(istream,
+				false);
+		ris.read(new byte[5]);
+		assertEquals("readed size", 5, ris.getSize());
+		ris.close();
+		assertEquals("in the end", 5, ris.getSize());
+	}
+
+	@org.junit.Test
+	public void simpleRead() throws Exception {
+		final InputStream istream = ReadSizeInputStreamTest.class
+				.getResourceAsStream("/testFile.txt");
+		final SizeReaderInputStream ris = new SizeReaderInputStream(istream);
+		assertEquals("readed size", 0, ris.getSize());
+		IOUtils.copy(ris, new NullOutputStream());
+		assertEquals("in the end", 30, ris.getSize());
+	}
+
+	@org.junit.Test
+	public void skip() throws Exception {
+		final InputStream istream = ReadSizeInputStreamTest.class
+				.getResourceAsStream("/testFile.txt");
+		final SizeReaderInputStream ris = new SizeReaderInputStream(istream,
+				true);
+		ris.read(new byte[5]);
+		assertEquals("readed size", 5, ris.getSize());
+		ris.skip(10);
+		assertEquals("readed size", 15, ris.getSize());
+		ris.close();
+		assertEquals("in the end", 30, ris.getSize());
+	}
+
+}
