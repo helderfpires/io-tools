9eb528122504856ca0e71f5f0bc10312200f68e9
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2008-12-21 22:57:55 +0000

    DiskBufferInputStream improvements & junits

--- trunk/easystream/src/main/java/com/gc/iotools/stream/is/DiskBufferedInputStream.java
+++ trunk/easystream/src/main/java/com/gc/iotools/stream/is/DiskBufferedInputStream.java
@@ -5,9 +5,17 @@ package com.gc.iotools.stream.is;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.SequenceInputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.gc.iotools.stream.utils.StreamUtils;
 
 /**
  * InputStream that guarantee the reset().
@@ -16,16 +24,29 @@ import java.io.OutputStream;
  * threshold size it copy it to a temporary file;
  * 
  * @author dvd.smnt
- *@since 1.0.9
+ * @since 1.0.9
  */
 public class DiskBufferedInputStream extends InputStream {
-	private final InputStream bis;
-	private File tempFile;
+	private static final Log log = LogFactory
+			.getLog(DiskBufferedInputStream.class);
+	private InputStream current;
+	/*
+	 * Position of cursor into the marked stream if 0 the cursor is at the
+	 * beginning
+	 */
+	private long currentPosition = 0;
 	private OutputStream fileOs;
-	private final int threshold;
-	private long markPos;
 	private boolean markForever = false;
-	private int markLimit = -1;
+	private int markLimit = 0;
+	/*
+	 * -1 no marking <br> 0 the mark is at the beginning of the marking stream
+	 * <br> >0 need a skip because the mark was in the middle of the marking
+	 * stream (multiple marks)<br>
+	 */
+	private long markPos = 0;
+	private InputStream original;
+	private File tempFile = null;
+	private final int threshold;
 
 	/**
 	 * 
@@ -33,19 +54,21 @@ public class DiskBufferedInputStream extends InputStream {
 	 * @param threshold
 	 */
 	public DiskBufferedInputStream(final InputStream bis, final int threshold) {
-		this.bis = new BufferedInputStream(bis);
+		this.original = bis;
+		this.current = bis;
 		this.threshold = threshold;
 	}
 
 	@Override
 	public int available() throws IOException {
-		return this.bis.available();
+		return this.current.available();
 	}
 
 	@Override
 	public void close() throws IOException {
 		cleanup();
-		this.bis.close();
+		this.current.close();
+		this.original.close();
 	}
 
 	@Override
@@ -53,8 +76,13 @@ public class DiskBufferedInputStream extends InputStream {
 		this.markForever = readlimit < 0;
 		final int effectiveReadLimit = (this.markForever ? this.threshold
 				: Math.min(readlimit, this.threshold));
-		this.bis.mark(effectiveReadLimit);
+		this.current = new BufferedInputStream(this.original);
+		this.current.mark(effectiveReadLimit);
 		this.markLimit = readlimit;
+		this.markPos = 0;
+		if (this.tempFile != null) {
+			this.markPos = this.currentPosition;
+		}
 	}
 
 	@Override
@@ -77,29 +105,87 @@ public class DiskBufferedInputStream extends InputStream {
 		} else if (len == 0) {
 			return 0;
 		}
-		if (this.markPos + len > this.threshold
-				&& this.markPos <= this.threshold) {
+		/*
+		 * occorre gestire il seguente caso: 1) lettura fino a threshold
+		 * (len=min(tres-currentPosition,len)) 2) quando len = treshold copia su
+		 * file.
+		 */
+		if (this.currentPosition == this.threshold) {
+			// switch to disk mode
 			this.tempFile = File.createTempFile("iotools-diskbufferis", ".tmp");
+			this.tempFile.deleteOnExit();
+			this.current.reset();
+			// TODO: better copy;
+			final byte[] buf = new byte[(int) this.currentPosition];
+			StreamUtils.tryReadFully(this.current, buf, 0,
+					(int) this.currentPosition);
+			this.fileOs = new FileOutputStream(this.tempFile);
+			this.fileOs.write(buf);
 		}
-		final int n = this.bis.read(b, off, len);
-		if (n > 0 && (this.markLimit != 0)) {
-			this.markPos += n;
+		int effectiveLen = len;
+		if (this.threshold - currentPosition > 0) {
+			effectiveLen = Math.min(len,
+					(int) (this.threshold - currentPosition));
+		}
+		final int n = this.current.read(b, off, effectiveLen);
+		if (((this.currentPosition + n) > this.markLimit)
+				&& (this.markLimit > 0)) {
+			// delete all marks
+			this.current = this.original;
+			cleanup();
+			markPos = -1;
+		}
+		if ((n > 0) && (this.markLimit != 0)) {
+			this.currentPosition += n;
+			if (this.fileOs != null) {
+				// copy the data to the file
+				this.fileOs.write(b, off, n);
+			}
 		}
 		return n;
 	}
 
 	@Override
 	public synchronized void reset() throws IOException {
-		// TODO Auto-generated method stub
-		super.reset();
+		if (this.current instanceof BufferedInputStream) {
+			this.current.reset();
+		} else if (this.fileOs != null) {
+			this.fileOs.close();
+			this.fileOs = null;
+			this.current = new SequenceInputStream(new FileInputStream(
+					this.tempFile), this.original);
+			this.original = this.current;
+			this.original.skip(this.markPos);
+		} else {
+			throw new IOException("why we're here?");
+		}
 	}
 
 	@Override
 	public long skip(final long n) throws IOException {
-		return this.bis.skip(n);
+		throw new UnsupportedOperationException("not yet implemented");
+		// return this.original.skip(n);
 	}
 
 	private void cleanup() {
+		if (this.fileOs != null) {
+			try {
+				this.fileOs.close();
+			} catch (final IOException e) {
+				DiskBufferedInputStream.log.debug("Exception closing the file",
+						e);
+			}
+			this.fileOs = null;
+		}
+
+		if (this.tempFile != null) {
+			final boolean del = this.tempFile.delete();
+			if (!del) {
+				DiskBufferedInputStream.log.warn("can not delete ["
+						+ this.tempFile.getName() + "]");
+			}
+			this.tempFile = null;
+		}
 
 	}
 
