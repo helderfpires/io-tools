f25682e86935e05c368eb83f837e65be87f8d3a1
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2009-01-21 09:34:02 +0000

    [maven-release-plugin]  copy for tag io-tools-1.1.2

---
+++ tags/io-tools-1.1.2/src/main/java/com/gc/iotools/fmt/base/Decoder.java
@@ -0,0 +1,17 @@
+package com.gc.iotools.fmt.base;
+
+
+/**
+ * Interface for extracting the content from an encoding format (es.Base64)
+ * 
+ * @author dvd.smnt
+ */
+public interface Decoder {
+	byte[] decode(byte[] encodedBytes);
+
+	int getEncodingOffset();
+
+	FormatEnum getFormat();
+
+	float getRatio();
+}
