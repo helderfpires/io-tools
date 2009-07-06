d0a4b820ea84410d64cdff344eab5417e81f023a
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2009-07-06 19:08:11 +0000

    javadocs

--- trunk/easystream/src/main/java/com/gc/iotools/stream/is/TeeInputStreamOutputStream.java
+++ trunk/easystream/src/main/java/com/gc/iotools/stream/is/TeeInputStreamOutputStream.java
@@ -20,17 +20,21 @@ import com.gc.iotools.stream.base.EasyStreamConstants;
  * <p>
  * When the method <code>{@link #close()}</code> is invoked all the bytes
  * remaining in the underlying <code>InputStream</code> are copied to the
- * <code>OutputStream</code>. This behavior is different from this class and
+ * <code>OutputStream(s)</code>. This behavior is different from this class and
  * {@code TeeInputStream} in Apache commons-io.
  * </p>
  * <p>
  * Bytes skipped are in any case copied to the <code>OutputStream</code>. Mark
  * and reset of the outer <code>InputStream</code> doesn't affect the data
- * copied to the <code>OutputStream</code>.
+ * copied to the <code>OutputStream</code>, that remain similar to the
+ * <code>InputStream</code> passed in the constructor.
  * </p>
  * <p>
- * It is also possible to copy the data to multiple <code>OutputStreams</code>
- * at once.
+ * It also calculate some statistics on the read/write operations.
+ * {@link #getWriteTime()} returns the time spent writing to the OutputStreams,
+ * {@link getReadTime()} returns the time spent reading from the InputStream and
+ * {@link TeeInputStreamOutputStream#getWriteSize()} returns the amount of data
+ * written to a single <code>OutputStream</code> until now.
  * </p>
  * <p>
  * Sample usage:
@@ -57,6 +61,10 @@ public class TeeInputStreamOutputStream extends AbstractInputStreamWrapper {
 
 	private final long[] writeTime;
 
+	private final long readTime = 0;
+
+	private final long writeSize = 0;
+
 	private long markPosition = 0;
 
 	private long destinationPosition = 0;
@@ -71,6 +79,51 @@ public class TeeInputStreamOutputStream extends AbstractInputStreamWrapper {
 	 * streams are closed when {@link #close()} is invoked.
 	 */
 	protected final boolean closeStreams;
+
+	/**
+	 * <p>
+	 * Creates a <code>TeeOutputStream</code> and saves its argument, the input
+	 * stream <code>source</code> and the output stream <code>destination</code>
+	 * for later use.
+	 * </p>
+	 * <p>
+	 * This constructor allow to specify multiple <code>OutputStream</code> to
+	 * which the data will be copied.
+	 * </p>
+	 * 
+	 * @since 1.2.3
+	 * @param source
+	 *            The underlying <code>InputStream</code>
+	 * @param closeCalled
+	 *            if <code>true</code> the <code>destination</code> will be
+	 *            closed when the {@link #close()} method is invoked. If
+	 *            <code>false</code> the close method on the underlying streams
+	 *            will not be called (it must be invoked externally).
+	 * @param destinations
+	 *            Data read from <code>source</code> are also written to this
+	 *            <code>OutputStream</code>.
+	 */
+	public TeeInputStreamOutputStream(final InputStream source,
+			final boolean closeStreams, final OutputStream... destinations) {
+		super(source);
+		if (destinations == null) {
+			throw new IllegalArgumentException(
+					"Destinations OutputStream can't be null");
+		}
+		if (destinations.length == 0) {
+			throw new IllegalArgumentException(
+					"At least one destination OutputStream must be specified");
+		}
+		for (final OutputStream destination : destinations) {
+			if (destination == null) {
+				throw new IllegalArgumentException(
+						"One of the outputstreams in the array is null");
+			}
+		}
+		this.writeTime = new long[destinations.length + 1];
+		this.destinations = destinations;
+		this.closeStreams = closeStreams;
+	}
 
 	/**
 	 * <p>
@@ -120,51 +173,6 @@ public class TeeInputStreamOutputStream extends AbstractInputStreamWrapper {
 	}
 
 	/**
-	 * <p>
-	 * Creates a <code>TeeOutputStream</code> and saves its argument, the input
-	 * stream <code>source</code> and the output stream <code>destination</code>
-	 * for later use.
-	 * </p>
-	 * <p>
-	 * This constructor allow to specify multiple <code>OutputStream</code> to
-	 * which the data will be copied.
-	 * </p>
-	 * 
-	 * @since 1.2.3
-	 * @param source
-	 *            The underlying <code>InputStream</code>
-	 * @param closeCalled
-	 *            if <code>true</code> the <code>destination</code> will be
-	 *            closed when the {@link #close()} method is invoked. If
-	 *            <code>false</code> the close method on the underlying streams
-	 *            will not be called (it must be invoked externally).
-	 * @param destinations
-	 *            Data read from <code>source</code> are also written to this
-	 *            <code>OutputStream</code>.
-	 */
-	public TeeInputStreamOutputStream(final InputStream source,
-			final boolean closeStreams, final OutputStream... destinations) {
-		super(source);
-		if (destinations == null) {
-			throw new IllegalArgumentException(
-					"Destinations OutputStream can't be null");
-		}
-		if (destinations.length == 0) {
-			throw new IllegalArgumentException(
-					"At least one destination OutputStream must be specified");
-		}
-		for (OutputStream destination : destinations) {
-			if (destination == null) {
-				throw new IllegalArgumentException(
-						"One of the outputstreams in the array is null");
-			}
-		}
-		this.writeTime = new long[destinations.length + 1];
-		this.destinations = destinations;
-		this.closeStreams = closeStreams;
-	}
-
-	/**
 	 * {@inheritDoc}
 	 */
 	@Override
@@ -207,9 +215,9 @@ public class TeeInputStreamOutputStream extends AbstractInputStreamWrapper {
 		}
 		if (this.closeStreams) {
 			this.source.close();
-			for (int i = 0; i < destinations.length; i++) {
-				long start = System.currentTimeMillis();
-				destinations[i].close();
+			for (int i = 0; i < this.destinations.length; i++) {
+				final long start = System.currentTimeMillis();
+				this.destinations[i].close();
 				getWriteTime()[i] += System.currentTimeMillis() - start;
 			}
 		}
@@ -218,17 +226,34 @@ public class TeeInputStreamOutputStream extends AbstractInputStreamWrapper {
 		}
 	}
 
+	/**
+	 * <p>
+	 * Return the time spent writing on the destination
+	 * <code>OutputStream(s)</code> in milliseconds.
+	 * </p>
+	 * <p>
+	 * The returned array has one element for each <code>OutputStream</code>
+	 * passed in the constructor.
+	 * </p>
+	 * 
+	 * @return time spent writing on the destination <code>OutputStreams</code>.
+	 */
+	public long[] getWriteTime() {
+		return this.writeTime;
+	}
+
 	@Override
 	public int innerRead(final byte[] b, final int off, final int len)
 			throws IOException {
+
 		final int result = this.source.read(b, off, len);
 		if (result > 0) {
 			if (this.sourcePosition + result > this.destinationPosition) {
 				final int newLen = (int) (this.sourcePosition + result - this.destinationPosition);
 				final int newOff = off + (result - newLen);
-				for (int i = 0; i < destinations.length; i++) {
-					long start = System.currentTimeMillis();
-					destinations[i].write(b, newOff, newLen);
+				for (int i = 0; i < this.destinations.length; i++) {
+					final long start = System.currentTimeMillis();
+					this.destinations[i].write(b, newOff, newLen);
 					getWriteTime()[i] += System.currentTimeMillis() - start;
 				}
 				this.destinationPosition += newLen;
@@ -275,9 +300,9 @@ public class TeeInputStreamOutputStream extends AbstractInputStreamWrapper {
 		if (result >= 0) {
 			this.sourcePosition++;
 			if (this.sourcePosition > this.destinationPosition) {
-				for (int i = 0; i < destinations.length; i++) {
-					long start = System.currentTimeMillis();
-					destinations[i].write(result);
+				for (int i = 0; i < this.destinations.length; i++) {
+					final long start = System.currentTimeMillis();
+					this.destinations[i].write(result);
 					getWriteTime()[i] += System.currentTimeMillis() - start;
 				}
 				this.destinationPosition++;
@@ -312,38 +337,4 @@ public class TeeInputStreamOutputStream extends AbstractInputStreamWrapper {
 		this.sourcePosition = this.markPosition;
 	}
 
-	/**
-	 * <p>
-	 * Return the time spent writing on the destination
-	 * <code>OutputStream(s)</code> in milliseconds.
-	 * </p>
-	 * <p>
-	 * The returned array has one element for each <code>OutputStream</code>
-	 * passed in the constructor.
-	 * </p>
-	 * 
-	 * @deprecated
-	 * @return time spent writing on the destination <code>OutputStreams</code>.
-	 * @see getStats
-	 */
-	@Deprecated
-	public long[] getWriteTime() {
-		return writeTime;
-	}
-
-	/**
-	 * <p>
-	 * Return an array of long representing the time spent reading from the
-	 * <code>InputStream</code> or writing on the destination
-	 * <code>OutputStream(s)</code>. Time unit is milliseconds.
-	 * </p>
-	 * <p>
-	 * The returned array has is has one element for each
-	 * <code>OutputStream</code> passed in the constructor.
-	 * </p>
-	 * 
-	 */
-	public long[] getTimeSpent() {
-		return writeTime;
-	}
 }
