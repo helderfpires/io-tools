2cd76541c442ec54894db74841eccec02762ccc5
dvd.smnt <dvd.smnt@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2009-11-05 22:49:59 +0000

    [maven-release-plugin]  copy for tag io-tools-1.2.4

---
+++ tags/io-tools-1.2.4/easystream/src/test/java/com/gc/iotools/stream/utils/StreamUtilsTest.java
@@ -0,0 +1,30 @@
+package com.gc.iotools.stream.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StreamUtilsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetBandWidthString() {
+		assertEquals("rate", "0.03 Byte/sec", StreamUtils.getRateString(3,
+				100000));
+		assertEquals("rate", "30 Byte/sec", StreamUtils.getRateString(30,
+				1000));
+		assertEquals("rate", "3.5 KB/sec", StreamUtils.getRateString(
+				(int) (1024 * 3.5), 1000));
+		assertEquals("rate", "35 KB/sec", StreamUtils.getRateString(
+				(1024 * 35), 1000));
+		assertEquals("rate", "3.5 MB/sec", StreamUtils.getRateString(
+				(int) (1024 * 1024 * 3.5), 1000));
+		assertEquals("rate", "3500 GB/sec", StreamUtils.getRateString(
+				1024L * 1024 * 1024 * 3500, 1000));
+	}
+
+}
