5f2757b05cd95a270c2b5a6c7727549716388b0f
dvd.smnt@gmail.com <dvd.smnt@gmail.com@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2010-09-18 15:26:50 +0000

    [maven-release-plugin]  copy for tag io-tools-1.2.8

---
+++ tags/io-tools-1.2.8/wazformat/src/main/java/uk/gov/nationalarchives/droid/base/SimpleElement.java
@@ -0,0 +1,55 @@
+/*
+ * c The National Archives 2005-2006.  All rights reserved.
+ * See Licence.txt for full licence details.
+ *
+ * Developed by:
+ * Tessella Support Services plc
+ * 3 Vineyard Chambers
+ * Abingdon, OX14 3PX
+ * United Kingdom
+ * http://www.tessella.com
+ *
+ * Tessella/NPD/4305
+ * PRONOM 4
+ *
+ * SimpleElement.java
+ *
+ */
+package uk.gov.nationalarchives.droid.base;
+
+/**
+ * holds the basic details of an element read from an XML file
+ * 
+ * @author Martin Waller
+ * @version 4.0.0
+ */
+public class SimpleElement {
+	String myText = "";
+
+	/**
+	 * method to be overridden in cases where the element content needs to be
+	 * specified only when the end of element tag is reached
+	 */
+	public void completeElementContent() {
+	}
+
+	public String getElementName() {
+		String className = this.getClass().getName();
+		className = className.substring(className.lastIndexOf(".") + 1);
+		return className;
+	}
+
+	/* getters */
+	public String getText() {
+		return this.myText;
+	}
+
+	public void setAttributeValue(final String name, final String value) {
+		MessageDisplay.unknownAttributeWarning(name, getElementName());
+	}
+
+	/* setters */
+	public void setText(final String theText) {
+		this.myText += theText;
+	}
+}
---
+++ tags/io-tools-1.2.8/wazformat/src/test/java/sandbox/IdentificationRule.java
@@ -0,0 +1,13 @@
+package sandbox;
+
+public class IdentificationRule {
+
+	public ByteRange[] requestedBytes(){
+		return null;
+		
+	}
+	
+	public boolean setByteRange(ByteRange br){
+		return false;
+	}
+}
