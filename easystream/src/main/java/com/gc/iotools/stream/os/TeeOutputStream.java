2081f501f8a8326199ab2d88277faad940af02e4
dvd.smnt@gmail.com <dvd.smnt@gmail.com@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2010-09-22 00:02:12 +0000

    switch on and off copy selectively

--- easystream/src/main/java/com/gc/iotools/stream/os/TeeOutputStream.java
+++ easystream/src/main/java/com/gc/iotools/stream/os/TeeOutputStream.java
@@ -6,6 +6,7 @@ package com.gc.iotools.stream.os;
  */
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 /**
  * <p>
@@ -40,7 +41,7 @@ public class TeeOutputStream extends OutputStream {
 	 * {@link #close()} has been invoked.
 	 */
 	protected boolean closeCalled = false;
-	private boolean copyEnabled = true;
+	private final boolean[] copyEnabled;
 	/**
 	 * The destination <code>OutputStream(s)</code> where data is written.
 	 */
@@ -61,13 +62,15 @@ public class TeeOutputStream extends OutputStream {
 	 * 
 	 * @since 1.2.4
 	 * @param destinations
-	 *            Data written to this<code>OutputStream</code> are copied to
+	 *            Data written to this <code>OutputStream</code> are copied to
 	 *            all the <code>destinations</code>.
 	 */
 	public TeeOutputStream(final OutputStream... destinations) {
 		checkDestinations(destinations);
 		this.writeTime = new long[destinations.length];
 		this.destinations = destinations;
+		this.copyEnabled = new boolean[destinations.length];
+		Arrays.fill(this.copyEnabled, true);
 	}
 
 	private void checkDestinations(final OutputStream... dests) {
@@ -103,22 +106,55 @@ public class TeeOutputStream extends OutputStream {
 		}
 	}
 
-	private boolean doCopy() {
-		return (!this.closeCalled) && this.copyEnabled;
-	}
-
 	/**
 	 * <p>
 	 * Allow to switch off the copy to the underlying streams. The copy is
 	 * enabled by default. Setting the parameter to false will disable the
-	 * copy.
+	 * copy to all the underlying streams at once.
+	 * </p>
+	 * <p>
+	 * If you need more fine grained control you should use
+	 * {@link #enableCopy(boolean[])} .
 	 * </p>
 	 * 
-	 * @param enabled
+	 * @since 1.2.8
+	 * @param enable
 	 *            whether to copy or not the bytes to the underlying stream.
 	 */
-	public final void enableCopy(final boolean enabled) {
-		this.copyEnabled = enabled;
+	public final void enableCopy(final boolean enable) {
+		Arrays.fill(this.copyEnabled, enable);
+	}
+
+	/**
+	 * <p>
+	 * Allow to switch off the copy to the underlying streams, selectively
+	 * enabling or disabling copy on some specific stream.
+	 * </p>
+	 * <p>
+	 * The copy is enabled by default. Each element in the array correspond to
+	 * an <code>OutputStream</code> passed in the constructor. If the
+	 * correspondent element in the array passed as a parameter is set to
+	 * <code>true</code> the copy will be enabled.It can be invoked multiple
+	 * times.
+	 * </p>
+	 * 
+	 * @since 1.2.9
+	 * @param enable
+	 *            whether to copy or not the bytes to the underlying stream.
+	 */
+	public final void enableCopy(final boolean[] enable) {
+		if (enable == null) {
+			throw new IllegalArgumentException("Enable array can't be null");
+		}
+		if (enable.length != this.copyEnabled.length) {
+			throw new IllegalArgumentException("Enable array must be of "
+					+ "the same size of the OutputStream array passed "
+					+ "in the constructor. Array size [" + enable.length
+					+ "] streams [" + this.copyEnabled.length + "]");
+		}
+		for (int i = 0; i < enable.length; i++) {
+			this.copyEnabled[i] = enable[i];
+		}
 	}
 
 	/**
@@ -187,14 +223,18 @@ public class TeeOutputStream extends OutputStream {
 		if (b == null) {
 			throw new NullPointerException("Array of bytes can't be null");
 		}
-		if (doCopy()) {
+		if (!this.closeCalled) {
 			for (int i = 0; i < this.destinations.length; i++) {
-				final OutputStream stream = this.destinations[i];
-				final long start = System.currentTimeMillis();
-				stream.write(b);
-				this.writeTime[i] += System.currentTimeMillis() - start;
+				if (this.copyEnabled[i]) {
+					final OutputStream stream = this.destinations[i];
+					final long start = System.currentTimeMillis();
+					stream.write(b);
+					this.writeTime[i] += System.currentTimeMillis() - start;
+				}
 			}
 			this.size += b.length;
+		} else {
+			throw new IOException("Stream already closed.");
 		}
 	}
 
@@ -207,14 +247,18 @@ public class TeeOutputStream extends OutputStream {
 		if (b == null) {
 			throw new NullPointerException("Array of bytes can't be null");
 		}
-		if (doCopy()) {
+		if (!this.closeCalled) {
 			for (int i = 0; i < this.destinations.length; i++) {
-				final OutputStream stream = this.destinations[i];
-				final long start = System.currentTimeMillis();
-				stream.write(b, off, len);
-				this.writeTime[i] += System.currentTimeMillis() - start;
+				if (this.copyEnabled[i]) {
+					final OutputStream stream = this.destinations[i];
+					final long start = System.currentTimeMillis();
+					stream.write(b, off, len);
+					this.writeTime[i] += System.currentTimeMillis() - start;
+				}
 			}
 			this.size += len;
+		} else {
+			throw new IOException("Stream already closed.");
 		}
 	}
 
@@ -223,15 +267,19 @@ public class TeeOutputStream extends OutputStream {
 	 */
 	@Override
 	public void write(final int b) throws IOException {
-		if (doCopy()) {
+		if (!this.closeCalled) {
 			for (int i = 0; i < this.destinations.length; i++) {
-				final OutputStream stream = this.destinations[i];
-				final long start = System.currentTimeMillis();
-				stream.write(b);
-				this.size++;
-				this.writeTime[i] += System.currentTimeMillis() - start;
+				if (this.copyEnabled[i]) {
+					final OutputStream stream = this.destinations[i];
+					final long start = System.currentTimeMillis();
+					stream.write(b);
+					this.size++;
+					this.writeTime[i] += System.currentTimeMillis() - start;
+				}
 			}
 			this.size++;
+		} else {
+			throw new IOException("Stream already closed.");
 		}
 	}
 }
