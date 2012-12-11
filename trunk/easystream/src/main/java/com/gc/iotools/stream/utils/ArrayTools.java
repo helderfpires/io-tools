2947d79f516f0785ab54c18e5a50a9116030ca45
gabriele.contini@gmail.com <gabriele.contini@gmail.com@6398b9ca-8fa2-11dd-b636-15230e51c9d0>
2012-12-11 09:58:26 +0000

    commons-lang -> commons-lang3 (part1: easystream)

--- trunk/easystream/src/main/java/com/gc/iotools/stream/utils/ArrayTools.java
+++ trunk/easystream/src/main/java/com/gc/iotools/stream/utils/ArrayTools.java
@@ -1,13 +1,15 @@
 package com.gc.iotools.stream.utils;
 
+import java.lang.reflect.Array;
+
 /*
- * Copyright (c) 2008,2011 Davide Simonetti. This source code is released
+ * Copyright (c) 2008,2013 Davide Simonetti. This source code is released
  * under the BSD License.
  */
 
 /**
  * Miscellaneous utilities for Arrays, i haven't found anywhere.
- *
+ * 
  * @author dvd.smnt
  * @since 1.0.9
  * @version $Id$
@@ -15,7 +17,7 @@ package com.gc.iotools.stream.utils;
 public final class ArrayTools {
 	/**
 	 * Find the index of the contained array in the src array.
-	 *
+	 * 
 	 * @param src
 	 *            Source array.
 	 * @param contained
@@ -40,6 +42,24 @@ public final class ArrayTools {
 			result = (found ? (pos - 1) : -1);
 		}
 		return result;
+	}
+
+	public static byte[] subarray(final byte[] array,
+			int startIndexInclusive, int endIndexExclusive) {
+		if (array == null) {
+			return null;
+		}
+		if (endIndexExclusive > array.length) {
+			endIndexExclusive = array.length;
+		}
+		int newSize = endIndexExclusive - startIndexInclusive;
+		if (newSize <= 0) {
+			return new byte[0];
+		}
+
+		byte[] subarray = new byte[newSize];
+		System.arraycopy(array, startIndexInclusive, subarray, 0, newSize);
+		return subarray;
 	}
 
 	/*
